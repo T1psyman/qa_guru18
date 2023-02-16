ccaa2801443a9b94b68f3776cc27122e14a27966
Egor Blinov <gblinoff@gmail.com>
2023-02-16 16:51:37 +0300

    create msg method for merge conflict

--- src/main/java/lucky/qa/Main.java
+++ src/main/java/lucky/qa/Main.java
@@ -2,10 +2,6 @@ package lucky.qa;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println(getMsg());
-    }
-
-    private static String getMsg(){
-        return "Allow world";
+        System.out.println("hello qQA");
     }
 }
